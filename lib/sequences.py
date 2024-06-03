960cb90edb52988054c706dd4fb8af70d14056d5
Maria Kamau <maria.kamau@student.moringaschool.com>
2024-06-03 13:02:54 +0300

    "Implemented print_fibonacci function to generate and print Fibonacci sequence of given length."

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fibonacci_sequnce = []
+    if length > 0:
+        fibonacci_sequnce.append(0)
+    if length >=2:
+        fibonacci_sequnce.append(1)
+        for i in range(2, length):
+            fibonacci_sequnce.append(fibonacci_sequnce[i-1] + fibonacci_sequnce[i-2])
+
+    print(fibonacci_sequnce)        
